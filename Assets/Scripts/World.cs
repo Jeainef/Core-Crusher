09c0d5434c3046e246cf87ec47f653f6d3f1311d
Jeainef <Jeainef@gmail.com>
2024-11-15 08:04:38 +0100

    Centered world

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -7,18 +7,21 @@ public class World : MonoBehaviour
 
 
     [SerializeField] private int ChunkRenderDistance;
+     [SerializeField] private int ChunkWorldSize;
     [SerializeField] public int ChunkWidth=12;
     [SerializeField] public readonly int ChunkHeight=12;
 
-    public Chunk[,] chunks = new Chunk[6,6];
+    public Chunk[,] chunks;
     public Material material;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0;i<6;i++){
-            for(int j=0;j<6;j++){
-               chunks[i,j] = new Chunk(new ChunkCoords(i,j),this);
+        chunks=new Chunk[ChunkRenderDistance,ChunkRenderDistance];
+
+        for(int i=-ChunkRenderDistance/2;i<ChunkRenderDistance/2;i++){
+            for(int j=-ChunkRenderDistance/2;j<ChunkRenderDistance/2;j++){
+               chunks[i+ChunkRenderDistance/2,j+ChunkRenderDistance/2] = new Chunk(new ChunkCoords(i,j),this);
             }
         }
         
@@ -33,6 +36,6 @@ public class World : MonoBehaviour
         return (pos.x>0 && pos.x<ChunkRenderDistance && pos.y>0 && pos.y<ChunkRenderDistance );
     }
     public bool IsVoxelInWorld(Vector3 pos){
-        return (pos.x>=0 && pos.x<ChunkRenderDistance*ChunkWidth && pos.z>=0 && pos.z<ChunkRenderDistance*ChunkWidth);
+        return (pos.x>=-ChunkRenderDistance/2*ChunkWidth  && pos.x<ChunkRenderDistance/2*ChunkWidth && pos.z>=-ChunkRenderDistance/2*ChunkWidth  && pos.z<ChunkRenderDistance/2*ChunkWidth);
     }
 }
